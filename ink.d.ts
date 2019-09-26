7406f27fcfd4a5f2a4718116ad1fc59285b36157
Simon Laux <mobile.info@simonlaux.de>
2019-09-26 02:47:15 +0200

    add story state

--- ink.d.ts
+++ ink.d.ts
@@ -1,8 +1,10 @@
 import { Story, InkList } from './engine/Story'
+import { StoryState } from './engine/StoryState';
 
 declare interface Inkjs {
     Story: typeof Story
     InkList: typeof InkList
+    StoryState: typeof StoryState
 }
 
 declare let inkjs: Inkjs
